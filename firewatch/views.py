718fab194f2da547ba8772eef3218b7e48bf88ff
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2025-02-10 11:04:40 +0530

    Add initial Django app structure with URL routing and templates

--- firewatch/views.py
+++ firewatch/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return render(request,"index.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def register(request):
+    return render(request,"register.html")
+
+def live(request):
+    return render(request,"live.html")
+
+def upload(request):
+    return render(request,"upload.html")
